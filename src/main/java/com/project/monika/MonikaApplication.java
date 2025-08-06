fd73c5b3490e560d049974510f6b9921245fde5d
Sampson Gbewa <Sampson.Gbewa@9psb.com.ng>
2025-08-06 13:16:10 +0100

    first commit

---
+++ src/main/java/com/project/monika/MonikaApplication.java
@@ -0,0 +1,13 @@
+package com.project.monika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/monika/MonikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.monika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
